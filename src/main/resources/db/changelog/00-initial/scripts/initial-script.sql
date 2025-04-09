8447b52c4c8e278d34ded85ade8977513db50d56
Robert Cabrera <clrobert.06@gmail.com>
2025-04-08 23:55:46 -0400

    feat(PPP-3): Add initial database schema

--- src/main/resources/db/changelog/00-initial/scripts/initial-script.sql
+++ src/main/resources/db/changelog/00-initial/scripts/initial-script.sql
@@ -35,124 +35,137 @@ DROP SEQUENCE IF EXISTS seq_configuration_id;
 CREATE SEQUENCE seq_configuration_id;
 
 -- Table: Person
-CREATE TABLE Person (
-    id INT DEFAULT nextval('seq_person_id') PRIMARY KEY,
-    ci VARCHAR(15) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone VARCHAR(15),
-    image VARCHAR(100),
-    gender VARCHAR(10),
-    birthdate DATE,
-    type VARCHAR(10),
-    address VARCHAR(100),
-    country VARCHAR(100),
-    role VARCHAR(50),
+CREATE TABLE Person
+(
+    id         INT       DEFAULT nextval('seq_person_id') PRIMARY KEY,
+    ci         VARCHAR(15)  NOT NULL,
+    name       VARCHAR(100) NOT NULL,
+    lastname   VARCHAR(100) NOT NULL,
+    email      VARCHAR(100) NOT NULL,
+    phone      VARCHAR(15),
+    image      VARCHAR(100),
+    gender     VARCHAR(10),
+    birthdate  DATE,
+    type       VARCHAR(10),
+    address    VARCHAR(100),
+    country    VARCHAR(100),
+    role       VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE UNIQUE INDEX idx_person_email ON Person(email);
+CREATE UNIQUE INDEX idx_person_email ON Person (email);
 
 -- Table: User
-CREATE TABLE User (
-    id INT DEFAULT nextval('seq_user_id') PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    role VARCHAR(50),
+CREATE TABLE Users
+(
+    id         INT       DEFAULT nextval('seq_user_id') PRIMARY KEY,
+    username   VARCHAR(100) NOT NULL,
+    email      VARCHAR(100) NOT NULL,
+    password   VARCHAR(50)  NOT NULL,
+    role       VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    person_id INT NOT NULL,
-    CONSTRAINT fk_user_person FOREIGN KEY (person_id) REFERENCES Person(id)
+    person_id  INT          NOT NULL,
+    CONSTRAINT fk_user_person FOREIGN KEY (person_id) REFERENCES Person (id)
 );
-CREATE UNIQUE INDEX idx_user_username ON User(username);
+CREATE UNIQUE INDEX idx_user_username ON Users (username);
 
 -- Table: Client
-CREATE TABLE Client (
-    id INT DEFAULT nextval('seq_client_id') PRIMARY KEY,
+CREATE TABLE Client
+(
+    id         INT       DEFAULT nextval('seq_client_id') PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    person_id INT NOT NULL,
-    CONSTRAINT fk_client_person FOREIGN KEY (person_id) REFERENCES Person(id)
+    person_id  INT NOT NULL,
+    CONSTRAINT fk_client_person FOREIGN KEY (person_id) REFERENCES Person (id)
 );
 
 -- Table: Staff
-CREATE TABLE Staff (
-    id INT DEFAULT nextval('seq_staff_id') PRIMARY KEY,
+CREATE TABLE Staff
+(
+    id         INT       DEFAULT nextval('seq_staff_id') PRIMARY KEY,
     profession VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    person_id INT NOT NULL,
-    CONSTRAINT fk_staff_person FOREIGN KEY (person_id) REFERENCES Person(id)
+    person_id  INT NOT NULL,
+    CONSTRAINT fk_staff_person FOREIGN KEY (person_id) REFERENCES Person (id)
 );
 
 -- Table: Pet
-CREATE TABLE Pet (
-    id INT DEFAULT nextval('seq_pet_id') PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    gender VARCHAR(50),
-    breed VARCHAR(50),
-    birthdate DATE,
-    image VARCHAR(100),
-    client_id INT NOT NULL,
+CREATE TABLE Pet
+(
+    id         INT       DEFAULT nextval('seq_pet_id') PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    gender     VARCHAR(50),
+    breed      VARCHAR(50),
+    birthdate  DATE,
+    image      VARCHAR(100),
+    client_id  INT          NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_pet_client FOREIGN KEY (client_id) REFERENCES Client(id)
+    CONSTRAINT fk_pet_client FOREIGN KEY (client_id) REFERENCES Client (id)
 );
 
 -- Table: Reservation
-CREATE TABLE Reservation (
-    id INT DEFAULT nextval('seq_reservation_id') PRIMARY KEY,
-    total FLOAT NOT NULL,
-    status VARCHAR(100),
+CREATE TABLE Reservation
+(
+    id             INT       DEFAULT nextval('seq_reservation_id') PRIMARY KEY,
+    total          FLOAT NOT NULL,
+    status         VARCHAR(100),
     payment_status VARCHAR(100),
-    due FLOAT,
-    payment_total FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    due            FLOAT,
+    payment_total  FLOAT,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Table: ReservationDetail
-CREATE TABLE ReservationDetail (
-    id INT DEFAULT nextval('seq_reservationdetail_id') PRIMARY KEY,
-    reservation_date DATE NOT NULL,
-    presence_status INT,
-    cost DECIMAL,
-    pet_id INT NOT NULL,
-    plan_id INT NOT NULL,
-    reservation_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_reservationdetail_pet FOREIGN KEY (pet_id) REFERENCES Pet(id),
-    CONSTRAINT fk_reservationdetail_plan FOREIGN KEY (plan_id) REFERENCES Plan(id),
-    CONSTRAINT fk_reservationdetail_reservation FOREIGN KEY (reservation_id) REFERENCES Reservation(id)
-);
 
 -- Table: Plan
-CREATE TABLE Plan (
-    id INT DEFAULT nextval('seq_plan_id') PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price FLOAT NOT NULL,
+CREATE TABLE Plan
+(
+    id         INT       DEFAULT nextval('seq_plan_id') PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
+    price      FLOAT        NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Table: ReservationDetail
+CREATE TABLE ReservationDetail
+(
+    id               INT       DEFAULT nextval('seq_reservationdetail_id') PRIMARY KEY,
+    reservation_date DATE NOT NULL,
+    presence_status  INT,
+    cost             DECIMAL,
+    pet_id           INT  NOT NULL,
+    plan_id          INT  NOT NULL,
+    reservation_id   INT  NOT NULL,
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_reservationdetail_pet FOREIGN KEY (pet_id) REFERENCES Pet (id),
+    CONSTRAINT fk_reservationdetail_plan FOREIGN KEY (plan_id) REFERENCES Plan (id),
+    CONSTRAINT fk_reservationdetail_reservation FOREIGN KEY (reservation_id) REFERENCES Reservation (id)
 );
 
 -- Table: PaymentMethod
-CREATE TABLE PaymentMethod (
-    id INT DEFAULT nextval('seq_paymentmethod_id') PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+CREATE TABLE PaymentMethod
+(
+    id         INT       DEFAULT nextval('seq_paymentmethod_id') PRIMARY KEY,
+    name       VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Table: Payment
-CREATE TABLE Payment (
-    id INT DEFAULT nextval('seq_payment_id') PRIMARY KEY,
-    total_mount FLOAT NOT NULL,
-    amount_paid FLOAT NOT NULL,
-    change FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    reservation_id INT NOT NULL,
-    payment_method_id INT NOT NULL,    
-    CONSTRAINT fk_payment_reservation FOREIGN KEY (reservation_id) REFERENCES Reservation(id),
-    CONSTRAINT fk_payment_payment_method FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod(id)
+CREATE TABLE Payment
+(
+    id                INT       DEFAULT nextval('seq_payment_id') PRIMARY KEY,
+    total_mount       FLOAT NOT NULL,
+    amount_paid       FLOAT NOT NULL,
+    change            FLOAT,
+    created_at        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    reservation_id    INT   NOT NULL,
+    payment_method_id INT   NOT NULL,
+    CONSTRAINT fk_payment_reservation FOREIGN KEY (reservation_id) REFERENCES Reservation (id),
+    CONSTRAINT fk_payment_payment_method FOREIGN KEY (payment_method_id) REFERENCES PaymentMethod (id)
 );
 
 -- Table: Configuration
-CREATE TABLE Configuration (
-    id INT DEFAULT nextval('seq_configuration_id') PRIMARY KEY,
-    capacity INT NOT NULL,
+CREATE TABLE Configuration
+(
+    id         INT       DEFAULT nextval('seq_configuration_id') PRIMARY KEY,
+    capacity   INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
