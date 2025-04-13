9adfb6af324e037c72ca1b946b24dd5fd914ec8b
Robert Cabrera <clrobert.06@gmail.com>
2025-04-13 18:19:45 -0400

    feat(PPP-68): fix cors

--- src/main/java/com/mibu/pawparadiseback/config/ApplicationProperties.java
+++ src/main/java/com/mibu/pawparadiseback/config/ApplicationProperties.java
@@ -2,10 +2,12 @@ package com.mibu.pawparadiseback.config;
 
 import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "spring")
 public class ApplicationProperties {
@@ -13,10 +15,12 @@ public class ApplicationProperties {
   private WebProperties web;
 
   @Getter
+  @Setter
   public static class WebProperties {
     private CorsProperties cors;
 
     @Getter
+    @Setter
     public static class CorsProperties {
       private List<String> allowedOrigins;
       private String allowedMethods;
