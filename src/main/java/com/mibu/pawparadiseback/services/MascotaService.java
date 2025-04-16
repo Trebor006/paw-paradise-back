f4a0778098323cb6294550b51c6db8431b3f3a96
Robert Cabrera <clrobert.06@gmail.com>
2025-04-15 23:20:20 -0400

    feat(PPP-63): Actualizar mascotas cliente por ci y mascotaId

--- src/main/java/com/mibu/pawparadiseback/services/MascotaService.java
+++ src/main/java/com/mibu/pawparadiseback/services/MascotaService.java
@@ -1,6 +1,7 @@
 package com.mibu.pawparadiseback.services;
 
 import com.mibu.pawparadiseback.services.dto.input.MascotaRequestDto;
+import com.mibu.pawparadiseback.services.dto.input.UpdateMascotaRequestDto;
 import com.mibu.pawparadiseback.services.dto.output.MascotaResponseDto;
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface MascotaService {
   MascotaResponseDto registrarMascota(MascotaRequestDto mascotaRequestDto);
 
   List<MascotaResponseDto> obtenerMascotasPorCliente(String ci);
+
+  MascotaResponseDto actualizarMascota(String ci, Long mascotaId, UpdateMascotaRequestDto updateMascotaRequestDto);
 }
