26aed55fcf7066d8e89e20344eb1eb85e28b9c2d
Robert Cabrera Lara <clrobert.06@gmail.com>
2025-04-19 20:46:35 -0400

    Deployt to test (#43)
    * feat(PPP-74): Add new endpoint to PetController to getPet by PetId (#35)
    
    * feat(PPP-74): Add new endpoint to PetController to getPet by PetId
    
    * feat(PPP-74): Add new endpoint to PetController to getPet by PetId
    
    * feat(PPP-74): Add new endpoint to PetController to getPet by PetId
    
    * Hotfix/fix ppp 75 error al registrar cliente devuelve person (#38)
    
    * feat(PPP-75): Add new endpoint to PetController to getPet by PetId
    
    * feat(PPP-75): Add new endpoint to PetController to getPet by PetId
    
    * feat(PPP-75): fix bug
    
    * feat(PPP-76): fix bug in Pet table (#39)
    
    * Hotfix/fix ppp 76 error al guardar imagen de masctoa (#42)
    
    * feat(PPP-76): fix bug in Pet table
    
    * feat(PPP-76): fix bug in Pet table
    
    * feat(PPP-76): fix bug in Pet table
    
    * feat(PPP-76): fix bug in Pet table
    
    * feat(PPP-76): fix bug in Pet table

--- src/main/java/com/mibu/pawparadiseback/exceptions/ClientNotFoundException.java
+++ src/main/java/com/mibu/pawparadiseback/exceptions/ClientNotFoundException.java
@@ -1,4 +1,4 @@
-package com.mibu.pawparadiseback.services.exceptions;
+package com.mibu.pawparadiseback.exceptions;
 
 public class ClientNotFoundException extends RuntimeException {
   public ClientNotFoundException(String message) {
